da75b125641d77ba499732315db7187a54a6486c
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-09 01:09:43 -0800

    Update ci_function.R
    fix colnames and rownames

--- src/ci_function.R
+++ src/ci_function.R
@@ -18,9 +18,6 @@ ci_function <- function(df,
   y <- enquo(response_col)
   df <- as.data.frame(df)
   
-  # print(y)
-  # print(!!y)
-  
   y <- df %>% pull(!!y)
   
   mean <- smean.cl.normal(y)[1]
@@ -30,16 +27,23 @@ ci_function <- function(df,
   df2 <- 
     data.frame(lwr = lwr, 
                mean = mean, 
-               upr = upr)
+               upr = upr) 
   
   rownames(df2) <- NULL 
   
+  colnames(df2)[1] <- paste0(enexpr(response_col), "_lwr")
+  colnames(df2)[2] <- paste0(enexpr(response_col), "_mean")
+  colnames(df2)[3] <- paste0(enexpr(response_col), "_upr")
+  
   return(df2)
-  
-  
 }
 
+
+
+#*********************************************************
 # fn test: 
 ci_function(df = df2.nest$data[1], 
-            response_col = y_deaths) # %>% str
+            response_col = y_deaths) 
 
+ci_function(df = df2.nest$data[1], 
+            response_col = y_discharges) 
